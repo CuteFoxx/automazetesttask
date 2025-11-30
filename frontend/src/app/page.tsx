8bef1417579955ce6c570abb97b5f8d067d8a6cd
Alice <alicepolishchuk1@gmail.com>
2025-11-30 21:36:36 +0200

    feat: add app title

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,6 +1,11 @@
-"use client";
-
 import TodoWrapper from "./components/todoWrapper";
+
+import type { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Todo app",
+  description: "This is test task created for automaze",
+};
 
 export default function Home() {
   return (
